246ae109438491aa77a35dfabc8f8e066d33f116
Fabrício Zimmerer Murta <fabricio@object.net>
2020-05-01 01:23:44 -0300

    Develops further utility functions in 'utils' module.
    Copies the de-asyncing methods from 'ftp' into 'utils', as they are also
    going to be used in the 'git' module.
    - errors(): prints out (and flushes by default) the error messages
      logged
    - log_err(): log another error message
    - pop_last_error(): pops the last error message from the error list
    - sync_call(): makes an asynchronous call sync, filling errors() with
      messages if fail, returning true/false on command completion/failure.
    - sync_call_numeric(): similar to sync_call() but returns a number;
      returns -1 if the method failed (so this is only useful for unsigned
      numbers!)
    
    Introduced the FileReader class for synchronous line-by-line file
    reading.
    - The class can be initialized with or without passing a file path to read
    initially.
    - open(): Files can be opened at any time by calling it (only one at a
      time, must close() before opening a new one).
    - read(): returns the open file's next line.
    - eof(): returns whether the file's end has been reached during the last
      read().
    - close(): closes the currently opened file and frees its allocated
      FD.
    - In order to implement private members in the class, the ES2019 target,
      matching NodeJS 12, is set in tsconfig.json.

--- utils.ts
+++ utils.ts
@@ -1,5 +1,50 @@
+import * as dasync from "deasync";
+import * as fs from "fs";
+
+let errorMessages: string[] = [];
+
 function empty(what : string, minlen = 1) : boolean {
   return what == undefined || what == null || what == "" || what.length < minlen;
+}
+
+function errors(clear = true) : string {
+  let errorList = errorMessages.join("\n");
+
+  if (clear) errorMessages = [];
+
+  return errorList;
+}
+
+function log_err(message: string) {
+  errorMessages.push(message);
+}
+
+function pop_last_error() {
+  return errorMessages.pop();
+}
+
+function sync_call(what : Promise<any>) : boolean {
+  let finished = false;
+  let succeeded = false;
+  what.then(() => { succeeded = true })
+    .catch(log_err)
+    .finally(() => { finished = true });
+
+  dasync.loopWhile(() => !finished);
+
+  return succeeded;
+}
+
+function sync_call_numeric(what : Promise<Number>) : Number {
+  let finished = false;
+  let result : Number = -1;
+  what.then((value) => { result = value })
+    .catch(log_err)
+    .finally(() => { finished = true });
+
+  dasync.loopWhile(() => !finished);
+
+  return result;
 }
 
 function trimSlashes(what: string) {
@@ -9,4 +54,86 @@ function trimSlashes(what: string) {
   return what;
 }
 
-export { empty, trimSlashes };
+class FileReader {
+  #rd_fd: number = -1;
+  #rd_eof: boolean = true;
+
+  open(path: string) : boolean {
+    if (!fs.existsSync(path)) {
+      log_err("Unable to fetch log for current git HEAD.");
+      return false;
+    }
+
+    this.#rd_fd = fs.openSync(path, "r");
+    this.#rd_eof = false;
+
+    return true;
+  };
+
+  constructor(path : string = "") {
+    if (path.length > 0) this.open(path);
+  }
+
+  read() : string {
+    let char: string;
+    let char1b: string;
+    let buf = Buffer.allocUnsafe(1);
+    let line = "";
+    let rawline = Buffer.allocUnsafe(8192);
+    let tmpbuf : Buffer;
+    let idx = 0;
+    let eof = true;
+
+    if (this.#rd_fd < 0) {
+      log_err("Unable to read next line: no file is open.");
+      return "";
+    }
+
+    while (fs.readSync(this.#rd_fd, buf, 0, buf.length, null)) {
+      char = buf.toString();
+
+      if (char == "\r") continue;
+      if (char == "\n") {
+        eof = false;
+        break;
+      } else {
+        // Allocate more buffer space if full.
+        if (idx >= rawline.length) {
+          tmpbuf = Buffer.allocUnsafe(rawline.length + 8192);
+          rawline.copy(tmpbuf);
+          rawline = tmpbuf;
+        }
+
+        buf.copy(rawline, idx);
+        idx += buf.length;
+      }
+    }
+
+    if (rawline.length > 0) {
+      tmpbuf = Buffer.allocUnsafe(idx - buf.length + 1);
+      rawline.copy(tmpbuf, 0, 0, idx - buf.length + 1);
+      line = tmpbuf.toString();
+    }
+
+    if (eof) this.#rd_eof = true;
+
+    return line;
+  }
+
+  eof() : boolean {
+    return this.#rd_eof;
+  }
+
+  close() : boolean {
+    if (this.#rd_fd < 0) {
+      log_err("Unable to finish file reading: no file is open.");
+      return false;
+    }
+
+    fs.closeSync(this.#rd_fd);
+
+    return true;
+  }
+}
+
+export { empty, errors, FileReader, log_err, pop_last_error, sync_call, sync_call_numeric, trimSlashes };
